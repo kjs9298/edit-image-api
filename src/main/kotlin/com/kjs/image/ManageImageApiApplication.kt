84df7a5e6f32acbacb8b6bfe63bf4487bf4f32c4
김지수/메세징플랫폼개발팀/NE <zisu.kim@nhnent.com>
2018-06-02 19:02:05 +0900

    #API-1 Create a project
    - Spring boot + kotlin

---
+++ src/main/kotlin/com/kjs/image/ManageImageApiApplication.kt
@@ -0,0 +1,11 @@
+package com.kjs.image
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManageImageApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ManageImageApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/kjs/image/ManageImageApiApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.kjs.image
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ManageImageApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
